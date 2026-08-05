1c1da638e9feb8014aa0f90330c5fa79ac7ea814
casperjwk <casperjwk@gmail.com>
2026-08-05 09:06:22 +0900

    first commit

---
+++ src/app/page.js
@@ -0,0 +1,19 @@
+import { createClient } from "@/utils/supabase/client";
+import Home from "./components/Home";
+
+export default async function Page() {
+  const supabase = await createClient();
+  const { data: projects, error } = await supabase.from("portfolio").select();
+  console.log(projects);
+
+  if (error) {
+    console.error("연결실패", error);
+    return <div>프로젝트 로드 실패</div>;
+  }
+
+  return (
+    <>
+      <Home data={projects} />
+    </>
+  );
+}
